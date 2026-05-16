ff7a02de17eb03e55c10826d5c06423abdbc7496
wtfemanu <os9777190@gmail.com>
2026-05-16 13:36:19 -0300

    FEAT8:campo de telefone adicionado ao motorista

--- core/models/motorista.py
+++ core/models/motorista.py
@@ -3,6 +3,7 @@ from django.db import models
 class Motorista (models.Model):
     nome = models.CharField(max_length=120)
     cnh = models.CharField(max_length=11, unique= True)
+    telefone = models.CharField (max_length=11, unique=False, blank= True, null= True)
 
     def __str__(self):
         return f"{self.id} {self.nome} {self.cnh}"
